cb77462164176184cb66508e46cb1d01dd2c3717
matheuefranco <matheusefranco@gmail.com>
2024-03-06 07:27:00 -0300

    Pilha Java Aula 2.

--- src/pilhajava/Pilha.java
+++ src/pilhajava/Pilha.java
@@ -8,5 +8,17 @@ public class Pilha<T> {
           elementos =(T[]) new Object[tamanho];
           this.topo = -1;
     }
+    public boolean isFull(){
+        return topo==elementos.length-1;
+        /*if(topo==elementos.length-1)
+            return true;
+        return false; */
+    }
+    public boolean isEmpty(){
+        // return this.topo==-1;
+        if(this.topo==-1)
+            return true;
+        return false;
+    }
     
 }
