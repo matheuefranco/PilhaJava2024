5b16e55a09ea4583ecc9d0f554e7635b5426d357
matheuefranco <matheusefranco@gmail.com>
2024-03-12 21:38:21 -0300

    Add classe Recorde

--- src/pilhajava/PilhaJava.java
+++ src/pilhajava/PilhaJava.java
@@ -1,5 +1,7 @@
 package pilhajava;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class PilhaJava {
@@ -12,8 +14,20 @@ public class PilhaJava {
         System.out.println("3. Mostrar topo");
         System.out.println("4. Mostrar pilha");
         System.out.println("0. Sair");
-        System.out.printf("Digite a opção desejada: %n", "UTF-8");
+        System.out.printf("Digite a op desejada: %n", "UTF-8");
         return scanner.nextInt();
+    }
+    public static void leiaRecorde(Recorde recorde){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nome:");
+        recorde.setNome(scanner.nextLine());
+        System.out.println("Data [dd/mm/aaaa]:");
+        String dataRecorde = scanner.nextLine();
+        LocalDate dataFormatada =  LocalDate.parse(dataRecorde,
+                DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        recorde.setData(dataFormatada);
+        System.out.println("Tempo:");
+        recorde.setTempo(scanner.nextDouble());
     }
 
     
@@ -22,16 +36,29 @@ public class PilhaJava {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Tamanho da pilha:");
         tamanho = scanner.nextInt();
-        Pilha<Integer> pilha = new Pilha<>(tamanho);
+        Pilha<Recorde> pilha = new Pilha<>(tamanho);
         do{
             op = mostrarMenu();
             switch(op){
-                case 1: System.out.println("Elemento:");
-                        elemento = scanner.nextInt();
-                        if(pilha.push(elemento)==true)
+                case 1: System.out.println("Dados do recorde:");
+                        Recorde novoRecorde = new Recorde();
+                        leiaRecorde(novoRecorde);
+                        if(pilha.push(novoRecorde)==true)
                              System.out.println("Insercao realizada");
                         else
                             System.out.println("Stack Overflow!");
+                        
+                break;
+                case 2: if(!pilha.isEmpty())
+                          System.out.println("Elemento removido:"+ pilha.pop());
+                        else
+                          System.out.println("Pilha vazia!");
+                        
+                break;
+                case 3: if(!pilha.isEmpty())
+                          System.out.println("Elemento do topo:"+ pilha.peek());
+                        else
+                          System.out.println("Pilha vazia!");
                         
                 break;
                 case 4:System.out.println("Elementos da pilha\n");
